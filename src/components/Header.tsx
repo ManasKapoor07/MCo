8c967ed379ec9a7f77bae9669cef30cb12f90505
mkapoor-datopic <mkapoor@datopic.com>
2025-08-27 15:54:33 +0530

    live product lisitng

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
     { label: "Home", path: "/", key: "home" },
     { label: "About", path: "/about", key: "about" },
     { label: "Categories", key: "categories" },
-    { label: "Shop", path: "/shop", key: "shop" },
+    { label: "Shop", path: "/shop"P, key: "shop" },
     { label: "Contact", path: "/contact", key: "contact" },
   ];
 
--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const BASE_URL = "http://127.0.0.1:8000/api";
+export const BASE_URL = "https://mcobackend.onrender.com/api";
 
 export const apiReducer = createApi({
   reducerPath: "rootApi",
